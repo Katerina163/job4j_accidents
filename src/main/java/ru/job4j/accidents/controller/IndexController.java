806eaef7458e77264c470c64e649b5fded429b7b
Katerina <Nefedova.Katia@yandex.ru>
2023-06-07 14:14:11 +0400

    реализация стартовой страницы

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,12 +1,14 @@
 package ru.job4j.accidents.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
     @GetMapping({"/", "/index"})
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
+        model.addAttribute("user", "имя пользователя");
         return "index";
     }
 }
