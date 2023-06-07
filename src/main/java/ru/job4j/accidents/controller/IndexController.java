b1d17c31325ffd3faa62b9fa9219defe12aa99fa
Katerina <Nefedova.Katia@yandex.ru>
2023-06-07 17:37:43 +0400

    добавление вложенного AccidentType

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -2,19 +2,29 @@ package ru.job4j.accidents.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.model.AccidentType;
 import ru.job4j.accidents.service.AccidentService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class IndexController {
     private AccidentService service;
 
+    private List<AccidentType> typeList;
+
     public IndexController(AccidentService memAccidentService) {
         service = memAccidentService;
+        typeList = new ArrayList<>();
+        typeList.add(new AccidentType(1, "Две машины"));
+        typeList.add(new AccidentType(2, "Машина и человек"));
+        typeList.add(new AccidentType(3, "Машина и велосипед"));
+        for (var accident : service.getList()) {
+            accident.setType(typeList.get(1));
+        }
     }
 
     @GetMapping({"/", "/index"})
@@ -24,7 +34,8 @@ public class IndexController {
     }
 
     @GetMapping("/save")
-    public String getCreatePage() {
+    public String getCreatePage(Model model) {
+        model.addAttribute("types", typeList);
         return "create";
     }
 
@@ -36,18 +47,23 @@ public class IndexController {
 
     @PostMapping("/save")
     public String save(@ModelAttribute Accident accident) {
+        int id = accident.getType().getId();
+        accident.setType(typeList.get(id - 1));
         service.create(accident);
         return "redirect:/index";
     }
 
     @GetMapping("/modify/{id}")
     public String getModifyPage(@PathVariable int id, Model model) {
-        model.addAttribute("accident", service.getAccient(id));
+        model.addAttribute("accident", service.getAccient(id))
+                .addAttribute("types", typeList);
         return "modify";
     }
 
     @PostMapping("/modify")
     public String modify(@ModelAttribute Accident accident) {
+        int id = accident.getType().getId();
+        accident.setType(typeList.get(id - 1));
         service.modify(accident);
         return "redirect:/index";
     }
