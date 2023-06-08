50e064eefca1b306f33605bc3713be17848439fb
Katerina <Nefedova.Katia@yandex.ru>
2023-06-08 14:21:36 +0400

    переименование Accident

--- src/main/java/ru/job4j/accidents/repository/AccidentRepository.java
+++ src/main/java/ru/job4j/accidents/repository/AccidentRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface AccidentRepository {
-    Optional<Accident> getAccient(int id);
+    Optional<Accident> getAccident(int id);
 
     List<Accident> getAll();
 
