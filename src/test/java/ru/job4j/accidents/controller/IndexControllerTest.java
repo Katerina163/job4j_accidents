78457f30b88cfa6df30283076d3795c56543352c
Katerina <Nefedova.Katia@yandex.ru>
2023-06-16 10:28:41 +0400

    test spring

--- src/test/java/ru/job4j/accidents/controller/IndexControllerTest.java
+++ src/test/java/ru/job4j/accidents/controller/IndexControllerTest.java
@@ -11,12 +11,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.accidents.Main;
 
-@ActiveProfiles("test")
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
+@TestPropertySource("/application-test.properties")
 class IndexControllerTest {
     @Autowired
     private MockMvc mockMvc;
--- src/test/java/ru/job4j/accidents/controller/RegControlTest.java
+++ src/test/java/ru/job4j/accidents/controller/RegControlTest.java
@@ -12,10 +12,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.accidents.Main;
 
-@ActiveProfiles("test")
+@TestPropertySource("/application-test.properties")
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
 class RegControlTest {
